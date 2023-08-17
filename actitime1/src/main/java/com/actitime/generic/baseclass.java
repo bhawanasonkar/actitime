607a1af8cc4605f059528eb7b6189fe741c47e8b
bhawa <bhawa@bhawana>
2023-08-17 16:24:28 +0530

    actitime

---
+++ actitime1/src/main/java/com/actitime/generic/baseclass.java
@@ -0,0 +1,13 @@
+package com.actitime.generic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class baseclass {
+	@Test
+	public void openbrowser() {
+		Reporter.log("open browser",true);
+		
+	}
+
+}
